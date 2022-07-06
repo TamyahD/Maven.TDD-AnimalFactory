5070fdd5f691a2a3e1014a0bb1e886f01163b3b1
Myah <myah@Zipcoders-MacBook-Pro-61.local>
2022-07-06 16:59:52 -0400

    created new instance of Dog with parameter fields

--- src/test/java/rocks/zipcodewilmington/DogTest.java
+++ src/test/java/rocks/zipcodewilmington/DogTest.java
@@ -1,20 +1,30 @@
 package rocks.zipcodewilmington;
 
 import org.junit.Assert;
+import org.junit.Before;
 import org.junit.Test;
+import rocks.zipcodewilmington.animals.Animal;
 import rocks.zipcodewilmington.animals.Dog;
+import rocks.zipcodewilmington.animals.animal_creation.AnimalFactory;
+
+import java.util.Date;
 
 /**
  * @author leon on 4/19/18.
  */
 public class DogTest {
     // TODO - Create tests for `new Dog(String name, Date birthDate, Integer id)`
-    // TODO - Create tests for `speak`
-    // TODO - Create tests for `setBirthDate(Date birthDate)`
-    // TODO - Create tests for `void eat(Food food)`
-    // TODO - Create tests for `Integer getId()`
-    // TODO - Create test to check Animal inheritance; google search `java instanceof keyword`
-    // TODO - Create test to check Mammal inheritance; google search `java instanceof keyword`
+    Dog d;
+
+    @Before
+    public void createDogInstance() {
+        String name = "Brian";
+        Date birthDate = new Date();
+        Integer dogID = 333;
+
+        Dog d = new Dog(name, birthDate, dogID);
+    }
+
     @Test
     public void setNameTest() {
         // Given (a name exists and a dog exists)
@@ -28,4 +38,30 @@ public class DogTest {
         String dogName = dog.getName();
         Assert.assertEquals(dogName, givenName);
     }
+
+/*
+    @Test
+    public void testSetName() {
+*/
+/*
+        String name="Snoopy";
+        Date birthdate = new Date(5555555);
+        Integer dogID=123;
+
+        Dog expectedNewDog = new Dog(name,birthdate,dogID);
+
+        Assert.assertEquals(expectedNewDog, AnimalFactory.createDog(name,birthdate));
+*//*
+
+    }
+*/
+
+
+
+    // TODO - Create tests for `speak`
+    // TODO - Create tests for `setBirthDate(Date birthDate)`
+    // TODO - Create tests for `void eat(Food food)`
+    // TODO - Create tests for `Integer getId()`
+    // TODO - Create test to check Animal inheritance; google search `java instanceof keyword`
+    // TODO - Create test to check Mammal inheritance; google search `java instanceof keyword`
 }
