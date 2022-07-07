e36f4f37e49e5d67999035fea64a3b27a7e2ce1f
Myah <myah@Zipcoders-MacBook-Pro-61.local>
2022-07-06 22:32:45 -0400

    test cases pass for Dog

--- src/test/java/rocks/zipcodewilmington/DogTest.java
+++ src/test/java/rocks/zipcodewilmington/DogTest.java
@@ -5,6 +5,7 @@ import org.junit.Before;
 import org.junit.Test;
 import rocks.zipcodewilmington.animals.Animal;
 import rocks.zipcodewilmington.animals.Dog;
+import rocks.zipcodewilmington.animals.Mammal;
 import rocks.zipcodewilmington.animals.animal_creation.AnimalFactory;
 
 import java.util.Date;
@@ -18,11 +19,10 @@ public class DogTest {
 
     @Before
     public void createDogInstance() {
-        String name = "Brian";
-        Date birthDate = new Date();
-        Integer dogID = 333;
-
-        Dog d = new Dog(name, birthDate, dogID);
+        String defName = "Brian";
+        Date defBirthDate = new Date(55555);
+        Integer defDogID = 333;
+        d = new Dog(defName, defBirthDate, defDogID);
     }
 
     @Test
@@ -40,10 +40,6 @@ public class DogTest {
     }
 
 /*
-    @Test
-    public void testSetName() {
-*/
-/*
         String name="Snoopy";
         Date birthdate = new Date(5555555);
         Integer dogID=123;
@@ -51,17 +47,75 @@ public class DogTest {
         Dog expectedNewDog = new Dog(name,birthdate,dogID);
 
         Assert.assertEquals(expectedNewDog, AnimalFactory.createDog(name,birthdate));
-*//*
-
-    }
 */
 
 
+    @Test
+    public void testDogSpeak() {
+        // TODO - Create tests for `speak`
+        //Given
+        Dog doggo = new Dog(null, null, null);
+        //When
+        String expectedDogSound= "bark!";
+        String doggoSound = doggo.speak();
+        //Then
+        Assert.assertEquals(expectedDogSound, doggoSound);
+    }
 
-    // TODO - Create tests for `speak`
-    // TODO - Create tests for `setBirthDate(Date birthDate)`
-    // TODO - Create tests for `void eat(Food food)`
-    // TODO - Create tests for `Integer getId()`
-    // TODO - Create test to check Animal inheritance; google search `java instanceof keyword`
-    // TODO - Create test to check Mammal inheritance; google search `java instanceof keyword`
+    @Test
+    public void testSetBirthDate() {
+        // TODO - Create tests for `setBirthDate(Date birthDate)`
+        //Given
+        Dog doggo = new Dog(null, null, null);
+        Date givenBD = new Date(55555);
+        //When
+        doggo.setBirthDate(givenBD);
+        Date expectedDoggoBD = doggo.getBirthDate();
+        //Then
+        Assert.assertEquals(expectedDoggoBD,doggo.getBirthDate());
+    }
+
+    @Test
+    public void testDogEat() {
+        // TODO - Create tests for `void eat(Food food)`
+        //Given
+        Food dogFood = new Food();
+        //When
+        d.eat(dogFood);
+        Integer expectedNumOfMealsEaten = 1;
+        //Then
+        Assert.assertEquals(expectedNumOfMealsEaten,d.getNumberOfMealsEaten());
+    }
+
+    @Test
+    public void testGetID() {
+        // TODO - Create tests for `Integer getId()`
+        //Given
+        //When
+        Integer expectedID = 333;
+        //Then
+        Assert.assertEquals(expectedID, d.getId());
+
+
+    }
+
+    @Test
+    public void testAnimalInstance() {
+        // TODO - Create test to check Animal inheritance; google search `java instanceof keyword`
+        //Given
+        //When
+        boolean expectedOutcome = true;
+        //Then
+        Assert.assertEquals(expectedOutcome, d instanceof Animal);
+    }
+
+    @Test
+    public void testMammalInstance() {
+        // TODO - Create test to check Mammal inheritance; google search `java instanceof keyword`
+        //Given
+        //When
+        boolean expectedOutcome = true;
+        //Then
+        Assert.assertEquals(expectedOutcome, d instanceof Mammal);
+    }
 }
